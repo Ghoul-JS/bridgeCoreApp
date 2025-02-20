a369cd3cccea51b83a67b65710644b2f84f42807
Ghoul-JS <jmanuelgut170@gmail.com>
2025-02-20 15:27:52 -0500

    feat: wpp messager

---
+++ components/Wpp/Wpp.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+import wpp from '../../public/whatsapp-fill.svg'
+import Image from "next/image"
+
+const Wpp = () => {
+    return (
+        <div className="fixed z-10 sm:right-10 right-5 bottom-5 transform transition-transform duration-300 hover:scale-150">
+            <Link href='https://wa.me/573168435020?text=Hola, dime en que te podemos ayudar!' target="_black">
+                <Image alt="wpp-image" src={wpp} width={50} height={50}/> 
+            </Link>
+        </div>
+    )
+}
+
+export default Wpp
